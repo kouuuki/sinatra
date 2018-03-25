51e7a5c69932d6f53f5370c3cc5470168f6e39f1
kouuuki <mark.web.888@gmail.com>
2018-03-26 08:26:57 +0900

    first commit

---
+++ myapp.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/hello/:name' do |n|
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  #"Hello #{params['name']}!"
+  "hello #{n}"
+end
+
+get '/' do
+  code = "<%= Time.now %>"
+  erb code
+end
+
+get '/tweet/new' do
+  'hello'
+end
